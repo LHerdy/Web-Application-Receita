c72b4d9606b888eb99c883f57799e752927ff867
Leandro Herdy <le.herdy@gmail.com>
2020-05-09 01:00:38 -0300

    Implementando class Pessoa, aperfeiçoando o admin e incluindo campo de busca.

--- receitas/models.py
+++ receitas/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 from datetime import datetime
-
+from pessoas.models import Pessoa
 
 # Create your models here.
 
 class Receita(models.Model):
+    pessoa = models.ForeignKey(Pessoa, on_delete=models.CASCADE)
     nome_receita = models.CharField(max_length=200)
     ingredientes = models.TextField()
     modo_preparo = models.TextField()
@@ -12,3 +13,8 @@ class Receita(models.Model):
     rendimento = models.CharField(max_length=100)
     categoria = models.CharField(max_length=100)
     date_time = models.DateTimeField(default=datetime.now, blank=True)
+    foto_receita = models.ImageField(upload_to='foto/%d/%m/%y', blank=True)
+    publicada = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.nome_receita
